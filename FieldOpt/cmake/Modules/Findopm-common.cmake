daa3e551bbe7c7f1e538b07b71277743f97d57be
bragesk <brage_sk@hotmail.com>
2018-06-17 11:56:20 +0200

    Ignore a couple of local paths

--- FieldOpt/cmake/Modules/Findopm-common.cmake
+++ FieldOpt/cmake/Modules/Findopm-common.cmake
@@ -7,18 +7,21 @@
 
 include(LibFindMacros)
 
+set(CMAKE_IGNORE_PATH "/usr/local/lib/x86_64-linux-gnu")
 libfind_package(ecl ecl)
 
 # Include dir
 find_path(opm-common_INCLUDE_DIR
   NAMES opm
-  PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/libraries/include
-)
+  #PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/libraries/include
+  #PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/libraries/include/opm/common
+    PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/opm-common/libraries/include
+        )
 
 # Finally the library itself
 find_library(opm-common_LIBRARY
   NAMES opmcommon
-  PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/libraries/lib
+  PATHS ${CMAKE_SOURCE_DIR}/ThirdParty/opm-common/libraries/lib
 )
 
 libfind_process(opm-common)
